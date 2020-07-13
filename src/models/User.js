42faddad867adcf270338d2360489fcdf4c8fc0c
Atiqur Rahman <atiqur@goava.com>
2020-07-13 14:54:05 +0600

    update cart functionality with mongoose done

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const Product = require('./Product');
+const Product = require("./Product");
 
 const UserSchema = new Schema({
   name: {
@@ -43,7 +43,7 @@ const UserSchema = new Schema({
         price: {
           type: Number,
           require: true,
-        }
+        },
       },
     ],
     total_price: {
@@ -53,7 +53,6 @@ const UserSchema = new Schema({
     },
   },
 });
-
 
 // Cart releted functionlities
 UserSchema.methods.addToCart = async function (product) {
@@ -75,7 +74,7 @@ UserSchema.methods.addToCart = async function (product) {
   }
 
   updatedCart.total_price = updatedCart.items.reduce((acc, curr, index) => {
-    return acc + (curr.price * curr.quantity);
+    return acc + curr.price * curr.quantity;
   }, 0);
   this.cart = updatedCart;
   try {
@@ -84,190 +83,33 @@ UserSchema.methods.addToCart = async function (product) {
     console.log("Error occured ===> ", err);
     return err;
   }
-}
+};
 
+UserSchema.methods.updateCart = async function (product_id, updatedQuantity) {
+  const cartProductIndex = this.cart.items.findIndex(
+    (cp) => cp._id.toString() === product_id.toString()
+  );
+  const updatedCart = { ...this.cart };
 
-  UserSchema.methods.updateCart = async function (product_id, updatedQuantity) {
-    const cartProductIndex = this.cart.items.findIndex(
-      (cp) => cp._id.toString() === product_id.toString()
-    );
-    const updatedCart = { ...this.cart };
+  if (updatedQuantity > 0) {
+    this.cart.items[cartProductIndex].quantity = updatedQuantity;
+  } else {
+    this.cart.items.splice(cartProductIndex, 1);
+  }
 
-    if (updatedQuantity > 0) {
-      this.cart.items[cartProductIndex].quantity = updatedQuantity;
-    } else {
-      this.cart.items.splice(cartProductIndex, 1);
-    }
-
-    this.cart.total_price = updatedCart.items.reduce((acc, curr, index) => {
-      return acc + curr.price * curr.quantity;
-    }, 0);
-    try {
-      // cart.items = updatedCart.items
-      this.save();
-    } catch (err) {
-      console.log("Error occured ===> ", err);
-      return err;
-    }
-  };
-
+  this.cart.total_price = updatedCart.items.reduce((acc, curr, index) => {
+    return acc + curr.price * curr.quantity;
+  }, 0);
+  try {
+    // cart.items = updatedCart.items
+    this.save();
+  } catch (err) {
+    console.log("Error occured ===> ", err);
+    return err;
+  }
+};
 
 module.exports = mongoose.model("Users", UserSchema);
-
-// class User {
-//   constructor(userName, email, password, contactNumber, role, id, cart) {
-//     this.user_name = userName;
-//     this.email = email;
-//     this.contact_number = contactNumber;
-//     this.password = password;
-//     this.role = role;
-//     this._id = id ? new mongoDb.ObjectId(id) : null;
-//     this.cart = cart.items ? cart : { items: [], total_price: 0.0 };
-//   }
-
-//   // User specific functionalities
-
-//   save = async () => {
-//     const DB = getDB();
-//     try {
-//       return await DB.collection("users").insertOne(this);
-//     } catch (err) {
-//       return err;
-//     }
-//   };
-
-//   static async fetchAll() {
-//     const DB = getDB();
-//     try {
-//       return await DB.collection("users").find().toArray();
-//     } catch (err) {
-//       return err;
-//     }
-//   }
-
-//   static async fetchSingle(userId) {
-//     const DB = getDB();
-//     try {
-//       const user = await DB.collection("users")
-//         .find({ _id: new mongoDb.ObjectId(userId) })
-//         .next();
-//       return user;
-//     } catch (err) {
-//       return err;
-//     }
-//   }
-
-//   async updateUser() {
-//     const DB = getDB();
-//     try {
-//       await DB.collection("users").updateOne(
-//         {
-//           _id: this._id,
-//         },
-//         {
-//           $set: this,
-//         }
-//       );
-//     } catch (err) {
-//       console.log("Err", err);
-//       return err;
-//     }
-//   }
-
-//   static async deleteUser(userId) {
-//     const DB = getDB();
-//     try {
-//       return await DB.collection("users").deleteOne({
-//         _id: new mongoDB.ObjectId(userId),
-//       });
-//     } catch (error) {
-//       return error;
-//     }
-//   }
-
-//   // Cart releted functionlities
-//   async addToCart(product) {
-//     const DB = getDB();
-//     const cartProductIndex = this.cart.items.findIndex(
-//       (cp) => cp._id.toString() === product._id.toString()
-//     );
-//     const updatedCart = { ...this.cart };
-//     if (cartProductIndex >= 0) {
-//       updatedCart.items[cartProductIndex].quantity++;
-//     } else {
-//       updatedCart.items.push({
-//         _id: product._id,
-//         price: product.price,
-//         quantity: 1,
-//       });
-//     }
-
-//     updatedCart.total_price = updatedCart.items.reduce((acc, curr, index) => {
-//       return acc + curr.price * curr.quantity;
-//     }, 0);
-//     try {
-//       await DB.collection("users").updateOne(
-//         { _id: this._id },
-//         {
-//           $set: { ...this, cart: updatedCart },
-//         }
-//       );
-//     } catch (err) {
-//       console.log("Error occured ===> ", err);
-//       return err;
-//     }
-//   }
-
-//   async getCart() {
-//     const DB = getDB();
-//     const productIds = this.cart.items.map((item) => item._id) || [];
-//     try {
-//       const products = await DB.collection("products")
-//         .find({ _id: { $in: productIds } })
-//         .toArray();
-//       products.map((item, index) => {
-//         item.quantity = this.cart.items[index].quantity;
-//       });
-//       return {
-//         items: products,
-//         total_price: this.cart.total_price,
-//         total_product: this.cart.items.reduce((acc, curr) => {
-//           return acc + curr.quantity;
-//         }, 0),
-//       };
-//     } catch (err) {
-//       return err;
-//     }
-//   }
-
-//   async updateCart(product_id, updatedQuantity) {
-//     const DB = getDB();
-//     const cartProductIndex = this.cart.items.findIndex(
-//       (cp) => cp._id.toString() === product_id.toString()
-//     );
-//     const updatedCart = { ...this.cart };
-
-//     if (updatedQuantity > 0) {
-//       updatedCart.items[cartProductIndex].quantity = updatedQuantity;
-//     } else {
-//       updatedCart.items.splice(cartProductIndex, 1);
-//     }
-
-//     updatedCart.total_price = updatedCart.items.reduce((acc, curr, index) => {
-//       return acc + curr.price * curr.quantity;
-//     }, 0);
-//     try {
-//       await DB.collection("users").updateOne(
-//         { _id: this._id },
-//         {
-//           $set: { ...this, cart: updatedCart },
-//         }
-//       );
-//     } catch (err) {
-//       console.log("Error occured ===> ", err);
-//       return err;
-//     }
-//   }
 
 //   // Order related functionalities
 //   async AddOrder() {
